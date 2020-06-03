4435c0b44972eec108e2d1026ac82e1ac6836f01
Sugandh Kumar <sugandhpatna95@gmail.com>
2020-06-04 00:03:31 +0530

    Created Repository called as title repository to get data from API

---
+++ app/src/main/java/com/example/kotlincoroutines/main/TitleRepository.kt
@@ -0,0 +1,69 @@
+package com.example.kotlincoroutines.main
+
+/**
+ * TitleRepository provides an interface to fetch a title or request a new one be generated.
+ *
+ * Repository modules handle data operations. They provide a clean API so that the rest of the app
+ * can retrieve this data easily. They know where to get the data from and what API calls to make
+ * when data is updated. You can consider repositories to be mediators between different data
+ * sources, in our case it mediates between a network API and an offline database cache.
+ */
+class TitleRepository(val network: MainNetwork, val titleDao: TitleDao) {
+
+    /**
+     * [LiveData] to load title.
+     *
+     * This is the main interface for loading a title. The title will be loaded from the offline
+     * cache.
+     *
+     * Observing this will not cause the title to be refreshed, use [TitleRepository.refreshTitleWithCallbacks]
+     * to refresh the title.
+     */
+    val title: LiveData<String?> = titleDao.titleLiveData.map { it?.title }
+
+
+    // TODO: Add coroutines-based `fun refreshTitle` here
+
+    /**
+     * Refresh the current title and save the results to the offline cache.
+     *
+     * This method does not return the new title. Use [TitleRepository.title] to observe
+     * the current tile.
+     */
+    fun refreshTitleWithCallbacks(titleRefreshCallback: TitleRefreshCallback) {
+        // This request will be run on a background thread by retrofit
+        BACKGROUND.submit {
+            try {
+                // Make network request using a blocking call
+                val result = network.fetchNextTitle().execute()
+                if (result.isSuccessful) {
+                    // Save it to database
+                    titleDao.insertTitle(Title(result.body()!!))
+                    // Inform the caller the refresh is completed
+                    titleRefreshCallback.onCompleted()
+                } else {
+                    // If it's not successful, inform the callback of the error
+                    titleRefreshCallback.onError(
+                        TitleRefreshError("Unable to refresh title", null))
+                }
+            } catch (cause: Throwable) {
+                // If anything throws an exception, inform the caller
+                titleRefreshCallback.onError(
+                    TitleRefreshError("Unable to refresh title", cause))
+            }
+        }
+    }
+}
+
+/**
+ * Thrown when there was a error fetching a new title
+ *
+ * @property message user ready error message
+ * @property cause the original cause of this exception
+ */
+class TitleRefreshError(message: String, cause: Throwable?) : Throwable(message, cause)
+
+interface TitleRefreshCallback {
+    fun onCompleted()
+    fun onError(cause: Throwable)
+}
