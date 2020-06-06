2e8872203dbf48909db453b859687425a4cd56b0
Sugandh Kumar <sugandhpatna95@gmail.com>
2020-06-06 21:13:02 +0530

    Created data model class Plant.kt and also created inline class GrowZone

--- advanced-coroutine/src/main/java/com/example/advanced_coroutine/Plant.kt
+++ advanced-coroutine/src/main/java/com/example/advanced_coroutine/Plant.kt
@@ -1,5 +1,9 @@
 package com.example.advanced_coroutine
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 @Entity(tableName = "plants")
 data class Plant(
     @PrimaryKey @ColumnInfo(name = "id") val plantId: String,
---
+++ advanced-coroutine/src/main/java/com/example/advanced_coroutine/ui/PlantAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.advanced_coroutine.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ListAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.advanced_coroutine.Plant
+
+/**
+ * Adapter for the [RecyclerView] in [PlantListFragment].
+ */
+class PlantAdapter : ListAdapter<Plant, RecyclerView.ViewHolder>(PlantDiffCallback()) {
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val plant = getItem(position)
+        (holder as PlantViewHolder).bind(plant)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return PlantViewHolder(
+            ListItemPlantBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false))
+    }
+
+    class PlantViewHolder(
+        private val binding: ListItemPlantBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: Plant) {
+            binding.apply {
+                plant = item
+                executePendingBindings()
+            }
+        }
+    }
+}
+
+private class PlantDiffCallback : DiffUtil.ItemCallback<Plant>() {
+
+    override fun areItemsTheSame(oldItem: Plant, newItem: Plant): Boolean {
+        return oldItem.plantId == newItem.plantId
+    }
+
+    override fun areContentsTheSame(oldItem: Plant, newItem: Plant): Boolean {
+        return oldItem == newItem
+    }
+}
