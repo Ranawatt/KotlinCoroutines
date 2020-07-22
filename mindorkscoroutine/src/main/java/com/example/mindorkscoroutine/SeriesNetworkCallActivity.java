00012b51a4732273e4590328f7482ab57661c550
Sugandh Kumar <sugandhpatna95@gmail.com>
2020-07-22 11:35:23 +0530

    working on coroutine series

---
+++ mindorkscoroutine/src/main/java/com/example/mindorkscoroutine/SeriesNetworkCallActivity.java
@@ -0,0 +1,14 @@
+package com.example.mindorkscoroutine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SeriesNetworkCallActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_series_network_call);
+    }
+}
