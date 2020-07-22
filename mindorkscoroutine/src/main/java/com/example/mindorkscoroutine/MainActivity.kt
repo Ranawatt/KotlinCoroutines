8752a7c5b65b229e63bf48f35a0491b271892fa6
Sugandh Kumar <sugandhpatna95@gmail.com>
2020-07-22 23:48:47 +0530

    working on coroutine series

---
+++ mindorkscoroutine/src/main/java/com/example/mindorkscoroutine/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.mindorkscoroutine
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun seriesNetworkCallActivity(view: View) {
+        startActivity(Intent(this, SeriesNetworkCallActivity::class.java))
+    }
+}
---
+++ mindorkscoroutine/src/main/java/com/example/mindorkscoroutine/SeriesNetworkCallActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mindorkscoroutine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SeriesNetworkCallActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_series_network_call)
+    }
+}
