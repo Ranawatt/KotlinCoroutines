6e91aff49ba208d4e9e84771c2e437f6473ca01a
Sugandh Kumar <sugandhpatna95@gmail.com>
2020-07-23 23:38:00 +0530

    created ApiUserAdapter.kt

---
+++ mindorkscoroutine/src/main/java/com/example/mindorkscoroutine/learn/base/ApiUserAdapter.kt
@@ -0,0 +1,6 @@
+package com.example.mindorkscoroutine.learn.base
+
+import androidx.recyclerview.widget.RecyclerView
+
+class ApiUserAdapter(private val users: ArrayList<ApiUser>): RecyclerView.Adapter<ApiUserAdapter.DataViewHolder> {
+}
