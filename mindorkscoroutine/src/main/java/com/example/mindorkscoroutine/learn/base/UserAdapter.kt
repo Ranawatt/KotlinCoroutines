d060e3a3f20b70fd2a98cd81ab1f942ddcc3ac17
Sugandh Kumar <sugandhpatna95@gmail.com>
2021-04-03 21:36:27 +0530

    created UserAdapter.kt for mindorks-coroutine module

--- mindorkscoroutine/src/main/java/com/example/mindorkscoroutine/learn/base/UserAdapter.kt
+++ mindorkscoroutine/src/main/java/com/example/mindorkscoroutine/learn/base/UserAdapter.kt
@@ -4,14 +4,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.mindorkscoroutine.R
 import com.example.mindorkscoroutine.data.local.entity.User
+import kotlinx.android.synthetic.main.item_layout.view.*
 
 class UserAdapter(private val users: ArrayList<User>) : RecyclerView.Adapter<UserAdapter.DataViewHolder>() {
 
     class DataViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         fun bind(user: User) {
-
+            itemView.textViewUserName.text = user.name
+            itemView.textViewUserEmail.text = user.email
+            Glide.with(itemView.imageViewAvatar.context)
+                .load(user.avatar)
+                .into(itemView.imageViewAvatar)
         }
 
     }
